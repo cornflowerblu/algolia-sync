076a75986f15a520f02f9af575a4185d533e4def
Roger Urich <rogerdurich@gmail.com>
2023-02-19 17:16:33 -0800

    add prettier

--- src/db.ts
+++ src/db.ts
@@ -1,24 +1,26 @@
 import { Client } from 'pg'
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
 export const pgClient = new Client({
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
-    user: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || 'postgres',
-    database: process.env.DB_NAME || 'postgres',
-    ssl: process.env.DB_SSL ? process.env.DB_SSL === 'true' : false,
-});
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+  user: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
+  database: process.env.DB_NAME || 'postgres',
+  ssl: process.env.DB_SSL ? process.env.DB_SSL === 'true' : false,
+})
 
 export const dbConnection = async () => {
-    try {
-        await pgClient.connect();
-        console.log('Connected to database');
-        const res = await pgClient.query('SELECT $1::text as message', ['Query Success!'])
-        console.log(res.rows[0].message)
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    await pgClient.connect()
+    console.log('Connected to database')
+    const res = await pgClient.query('SELECT $1::text as message', [
+      'Query Success!',
+    ])
+    console.log(res.rows[0].message)
+  } catch (error) {
+    console.log(error)
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,36 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
-import algoliasearch from 'algoliasearch';
+import express, { Express, Request, Response } from 'express'
+import dotenv from 'dotenv'
+import algoliasearch from 'algoliasearch'
 import { dbConnection, pgClient } from './db'
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED || '1'
 
-dotenv.config();
+dotenv.config()
 
-const app: Express = express();
-const port = process.env.PORT || 8080;
+const app: Express = express()
+const port = process.env.PORT || 8080
 
 app.get('/health', (req: Request, res: Response) => {
-    res.json({
-        timestamp: Date.now(),
-        status: res.statusCode
-    })
-});
+  res.json({
+    timestamp: Date.now(),
+    status: res.statusCode,
+  })
+})
 
 app.get('/sync', async (req: Request, res: Response) => {
-
-    if (req.query.key !== process.env.SYNC_KEY) return res.status(401).json({
-        message: 'Unauthorized',
-        status: res.statusCode
+  if (req.query.key !== process.env.SYNC_KEY)
+    return res.status(401).json({
+      message: 'Unauthorized',
+      status: res.statusCode,
     })
 
-    const client = algoliasearch(process.env.ALG_APP_ID || '', process.env.ALG_API_KEY || '');
-    const index = client.initIndex('episodes');
+  const client = algoliasearch(
+    process.env.ALG_APP_ID || '',
+    process.env.ALG_API_KEY || '',
+  )
+  const index = client.initIndex('episodes')
 
-    const data = await pgClient.query(`
+  const data = await pgClient.query(`
         select episodes.id, episodes.name as name, episodes.description as description, s.name as show, s2.season_number, episode_number from episodes
         join episodes_show_links esl on episodes.id = esl.episode_id
         join episodes_season_links e on episodes.id = e.episode_id
@@ -36,26 +39,26 @@ app.get('/sync', async (req: Request, res: Response) => {
         order by show, season_number, episode_number asc;
     `)
 
-    const episodes = data.rows.map((row: any) => {
-        return {
-            objectID: row.id,
-            name: row.name,
-            description: row.description,
-            show: row.show,
-            season: row.season_number,
-            episode: row.episode_number
-        }
-    });
+  const episodes = data.rows.map((row: any) => {
+    return {
+      objectID: row.id,
+      name: row.name,
+      description: row.description,
+      show: row.show,
+      season: row.season_number,
+      episode: row.episode_number,
+    }
+  })
 
-    await index.saveObjects(episodes, { autoGenerateObjectIDIfNotExist: true });
-    res.json({
-        message: 'Synced',
-        data: episodes,
-        status: res.statusCode
-    })
+  await index.saveObjects(episodes, { autoGenerateObjectIDIfNotExist: true })
+  res.json({
+    message: 'Synced',
+    data: episodes,
+    status: res.statusCode,
+  })
 })
 
 app.listen(port, async () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-    await dbConnection();
-});
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
+  await dbConnection()
+})
