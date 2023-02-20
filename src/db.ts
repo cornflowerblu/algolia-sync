075ecfe0a5184d89dd1911234cbc5e0596ab92bf
Roger Urich <rogerdurich@gmail.com>
2023-02-19 19:05:09 -0800

    try to spin up postgres

--- src/db.ts
+++ src/db.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const pgClient = new Client({
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || 'postgres',
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
   user: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
