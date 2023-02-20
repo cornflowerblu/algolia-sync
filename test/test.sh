2c70123428e61257dfdfa2879e49064e43074683
Roger Urich <rogerdurich@gmail.com>
2023-02-20 02:11:25 -0800

    ohhhh yea

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,23 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+exitCodeArray=()
+
+onFailure() {
+    exitCodeArray+=( "$?" )
+}
+
+trap onFailure ERR
 
 npm run test
 
-exit 0
+addNumbers () {
+    local IFS='+'
+    printf "%s" "$(( $* ))"
+}
+
+trap '' ERR
+
+if (( $(addNumbers "${exitCodeArray[@]}") )); then
+    printf 'some of your tests failed\n' >&2
+    exit -1
+fi
