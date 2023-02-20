1a61115a15e15297c374ae2b288b591f00db6610
Roger Urich <rogerdurich@gmail.com>
2023-02-19 21:39:10 -0800

    this could be it!

--- src/main.test.ts
+++ src/main.test.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const requestWithSupertest = supertest(`http://localhost:${process.env.PORT}`)
+const requestWithSupertest = supertest(`http://0.0.0.0:${process.env.PORT}`)
 
 describe('Health check', () => {
     it('should return 200', async () => {
